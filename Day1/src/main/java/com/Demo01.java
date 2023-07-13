c51ec5bc0778952e4263458d67cbcb06caf6d94e
Jove <gc8580@outlook.com>
2023-07-13 13:30:36 +0800

    测试github

---
+++ Day1/src/main/java/com/Demo01.java
@@ -0,0 +1,18 @@
+package com;
+
+/**
+ * @BelongsProject: Day1
+ * @BelongsPackage: com
+ * @Author: Jove
+ * @CreateTime: 2023-07-13  13:22
+ * @Description: TODO
+ */
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("第一天");
+        System.out.println("测试github");
+    }
+}
+
+
---
+++ Day1/src/main/java/com/test.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * @BelongsProject: Day1
+ * @BelongsPackage: com
+ * @Author: Jove
+ * @CreateTime: 2023-07-13  12:24
+ * @Description: TODO
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
+
+
